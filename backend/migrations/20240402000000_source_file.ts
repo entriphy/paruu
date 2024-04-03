b8f96e3e8fde30d00e9aaa19ce3735ab0a7f3257
entriphy <1747088+entriphy@users.noreply.github.com>
2024-04-03 13:53:45 -0700

    backend: Add constraint for source_file column

--- backend/migrations/20240402000000_source_file.ts
+++ backend/migrations/20240402000000_source_file.ts
@@ -1,9 +1,17 @@
-import { Kysely } from "kysely";
+import { Kysely, sql } from "kysely";
 
 export async function up(db: Kysely<any>): Promise<void> {
-    await db.schema.alterTable("entry").addColumn("source_file", "varchar").execute();
+    await db.schema.alterTable("entry")
+        .addColumn("source_file", "varchar")
+        .execute();
+    await db.schema.alterTable("entry")
+        .addCheckConstraint("check_src", sql`
+            implemented = TRUE AND source_file IS NOT NULL
+        `)
+        .execute();
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
+    await db.schema.alterTable("entry").dropConstraint("check_src").execute();
     await db.schema.alterTable("entry").dropColumn("source_file").execute();
 }
